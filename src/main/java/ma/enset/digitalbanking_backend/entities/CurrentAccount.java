fde6de69661a7f69b6013e4e24f89928fd0273af
kawtar-collab <kawtar.khallouq-etu@etu.univh2c.ma>
2024-05-12 12:12:16 +0100

    create classes

--- src/main/java/ma/enset/digitalbanking_backend/entities/CurrentAccount.java
+++ src/main/java/ma/enset/digitalbanking_backend/entities/CurrentAccount.java
@@ -1,4 +1,17 @@
 package ma.enset.digitalbanking_backend.entities;
 
-public class CurrentAccount {
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@DiscriminatorValue("CA")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrentAccount extends BankAccount{
+    private double overDraft;
+
 }
